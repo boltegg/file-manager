5a5eaf1349ac594ca3e3e98653982f263bd7501d
BoLTeGG <epahan4ik@gmail.com>
2016-03-17 12:37:49 +0200

    add example

---
+++ example.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//include whois class
+include(__DIR__ . '\src\FileManager.php');
+$fileManager = new \FileManager();
+
+$path = 'C:/hello/test.txt';
+$content = 'blablabla';
+
+$result = $fileManager->write($path, $content);
+
+var_dump($result);
